f3d5ec4bca9aeba52fda35f6085942525998a0b1
Administrator <m@3ko.fr>
2015-10-16 21:00:23 +0200

    test ci

--- bashbrew/gitlab-ci.sh
+++ bashbrew/gitlab-ci.sh
@@ -5,22 +5,15 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 repos=( --all )
 
-upstreamRepo='alpine-library/alpinelib-images'
+upstreamRepo='alpine-library/official-images'
 upstreamBranch='master'
-if [ "$TRAVIS_PULL_REQUEST" -a "$TRAVIS_PULL_REQUEST" != 'false' ]; then
-	upstreamRepo="$TRAVIS_REPO_SLUG"
-	upstreamBranch="$TRAVIS_BRANCH"
-fi
 
 HEAD="$(git rev-parse --verify HEAD)"
 
 git fetch -q "https://github.com/$upstreamRepo.git" "refs/heads/$upstreamBranch"
 UPSTREAM="$(git rev-parse --verify FETCH_HEAD)"
 
-if [ "$TRAVIS_BRANCH" = 'master' -a "$TRAVIS_PULL_REQUEST" = 'false' ]; then
-	# if we're testing master itself, RUN ALL THE THINGS
-	echo >&2 'Testing master -- BUILD ALL THE THINGS!'
-elif [ "$(git diff --numstat "$UPSTREAM...$HEAD" -- . | wc -l)" -ne 0 ]; then
+if [ "$(git diff --numstat "$UPSTREAM...$HEAD" -- . | wc -l)" -ne 0 ]; then
 	# changes in bashbrew/ -- keep "--all" so we test the bashbrew script changes appropriately
 	echo >&2 'Changes in bashbrew/ detected!'
 else
